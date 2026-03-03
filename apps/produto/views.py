503572a14fcd78c8bf484d672064e1a46114b03c
Erik Diniz <erikdinizbeserra@gmail.com>
2026-03-03 20:03:59 -0300

    feat: update permissions in ProdutoViewSet to allow public access for list and retrieve actions

--- apps/produto/views.py
+++ apps/produto/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from drf_spectacular.utils import extend_schema
 
 from .models import Categoria, Produto
@@ -19,7 +19,7 @@ class ProdutoViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action in ["list", "retrieve"]:
-            return []
+            return [AllowAny()]
         else:
             return [IsAuthenticated()]
 
