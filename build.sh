71d5feff41c6ff6d63420872bb433ec03aaf4f24
Erik Diniz <erikdinizbeserra@gmail.com>
2026-03-13 20:55:52 -0300

    feat: add superuser creation script to build process for initial setup

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from apps.user.models import User
+if not User.objects.filter(email='admin@compia.com').exists():
+    User.objects.create_superuser(email='admin@compia.com', password='admin@123', nome='Admin', role='BACKOFFICE')
+"
