9cc4564afea7ac558458b38adc1f998546227c80
Erik Diniz <erikdinizbeserra@gmail.com>
2026-03-13 21:14:46 -0300

    feat: update Whitenoise storage backend to use CompressedStaticFilesStorage

--- compia/settings.py
+++ compia/settings.py
@@ -277,6 +277,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
